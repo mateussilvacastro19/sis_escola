0d84050da06effcd3af54c6be13d83775acce6bf
DTI CREA-AM <dti@crea-am.org.br>
2024-03-19 12:27:34 -0400

    inicio

---
+++ aluno.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+CADASTRO DE ALUNO
+<br>
+<br>
+
+<!--
+    comentario
+-->
+
+<form method="post">
+    Nome:<br>
+    <input type="text" name="alu_nome">
+    <br>
+    <br>
+
+    Idade:<br>
+    <input type="number" name="alu_idade">
+
+    <br>
+    <br>
+
+    <input type="submit" value="SALVAR">
+</form>
+<br>
+<a href="index.php">Home</a>
+
+</body>
+</html>
